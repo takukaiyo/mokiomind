724c20d257b4b29c0ed81151146501e739d57626
叶卓海 <165025136+takukaiyo@users.noreply.github.com>
2026-03-25 17:24:58 +0800

    init project

---
+++ dataset/lm_dataset.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2026/3/25 16:10
+# @Author  : yezhuohai
+# @File    : lm_dataset.py
+# @Software: PyCharm
---
+++ trainer/trainer_utils.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2026/3/25 16:11
+# @Author  : yezhuohai
+# @File    : trainer_utils.py
+# @Software: PyCharm
